7e1b110e6a0d348f25fcb6497d7c0a2cefd2fec9
Hasan El Jabir <eljabirhasan@gmail.com>
2019-04-02 06:32:06 +0700

    Check if new currency list screen if functioning

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Home from './screens/Home';
+// import Home from './screens/Home';
+import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
@@ -12,4 +13,5 @@ EStyleSheet.build({
   $inputText: '#797979',
 });
 
-export default () => <Home />;
+// export default () => <Home />;
+export default () => <CurrencyList />;
